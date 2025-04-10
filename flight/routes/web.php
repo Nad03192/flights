b08776326c083460d7351950da0649e4fa26361b
Your Name <nadinekeyrouz0011@gmail.com>
2025-04-10 11:45:24 +0300

    work

--- flight/routes/web.php
+++ flight/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PassengerController;
 use App\Http\Controllers\FlightController;
+use App\Http\Controllers\AuthController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -20,3 +21,14 @@ Route::resource('passengers', PassengerController::class);
 Route::resource('flights', FlightController::class);
 Route::put('flights/restore/{id}', [FlightController::class, 'restore'])->name('flights.restore');
 Route::get('flights/{flight}/passengers', [FlightController::class, 'showPassengers'])->name('flights.passengers');
+
+Route::get('register', [AuthController::class, 'showRegisterForm'])->name('register.form');
+Route::post('register', [AuthController::class, 'register'])->name('register');
+
+Route::get('login', [AuthController::class, 'showLoginForm'])->name('login.form');
+Route::post('login', [AuthController::class, 'login'])->name('login');
+
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('flightslist', [AuthController::class, 'listFlights'])->name('flights.list');
+Route::post('flights/{id}/book', [AuthController::class, 'bookFlight'])->name('flights.book');
