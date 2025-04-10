83a9b743cc80a5a8f4764730b3dfab5d7c0bbaea
Your Name <nadinekeyrouz0011@gmail.com>
2025-04-10 12:49:24 +0300

    pagination

--- flight/resources/views/passengers/index.blade.php
+++ flight/resources/views/passengers/index.blade.php
@@ -1,19 +1,10 @@
-<!-- resources/views/passengers/index.blade.php -->
-<h1>Passengers</h1>
-
-<!-- Search Form -->
-<form action="{{ route('passengers.search') }}" method="GET">
-    <input type="text" name="search" placeholder="Search passengers" value="{{ old('search', $searchTerm ?? '') }}" required>
-    <button type="submit">Search</button>
+<form method="GET" action="{{ route('passengers.index') }}">
+    <label for="perPage">Results per page:</label>
+    <input type="number" name="perPage" value="{{ request()->input('perPage', 15) }}" min="1">
+    <button type="submit">Apply</button>
 </form>
 
-
-<a href="{{ route('passengers.create') }}">Add New Passenger</a>
-
-<!-- Display message if no passengers found -->
-@if($passengers->isEmpty())
-    <p>No passengers found.</p>
-@else
+@if($passengers->count())
     <table>
         <thead>
             <tr>
@@ -24,29 +15,27 @@
             </tr>
         </thead>
         <tbody>
-            @foreach ($passengers as $passenger)
+            @foreach($passengers as $passenger)
                 <tr>
                     <td>{{ $passenger->first_name }}</td>
                     <td>{{ $passenger->last_name }}</td>
                     <td>{{ $passenger->email }}</td>
                     <td>
-                        @if ($passenger->trashed())
-                            <form action="{{ route('passengers.restore', $passenger->id) }}" method="POST">
-                                @csrf
-                                @method('PUT')
-                                <button type="submit">Restore</button>
-                            </form>
-                        @else
-                            <a href="{{ route('passengers.edit', $passenger->id) }}">Edit</a>
-                            <form action="{{ route('passengers.destroy', $passenger->id) }}" method="POST" style="display:inline;">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit">Delete</button>
-                            </form>
-                        @endif
+                        <a href="{{ route('passengers.edit', $passenger->id) }}">Edit</a>
+                        <form action="{{ route('passengers.destroy', $passenger->id) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Delete</button>
+                        </form>
                     </td>
                 </tr>
             @endforeach
         </tbody>
     </table>
+
+    <div>
+        {{ $passengers->links() }}
+    </div>
+@else
+    <p>No passengers found</p>
 @endif
